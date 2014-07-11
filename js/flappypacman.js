494459a58cc1e12334f5fbeda498576a503ff7db
Liliana Kastilio <lili2311@inbox.ru>
2014-07-11 13:03:51 +0100

    Update flappypacman.js

--- js/flappypacman.js
+++ js/flappypacman.js
@@ -3,15 +3,41 @@ var canvas, ctx, pacman = [], isPaused=false, score=0, tick=0, food, headfwd, cu
 
 canvas = document.getElementById('myCanvas');
 
-/**(function() {
+//create the particles
+
+var ps = [];
+var MAX_NUM = 500;
+var colors = [ '#69D2E7', '#A7DBD8', '#E0E4CC', '#F38630', '#FA6900', '#FF4E50', '#F9D423' ];
+
+function spawn() {
+  console.log("|-----spawm()")
+  for(var i=0; ps.length < MAX_NUM; i++) {
+    ps[i] = { x: Math.random()*window.innerWidth,
+              y: Math.random()*window.innerHeight,
+              r: Math.random()*5,
+              c: colors[Math.floor(Math.random()*colors.length)]
+            };                  
+   }
+  update();
+}
+
+
+
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+ 
+// requestAnimationFrame polyfill by Erik Möller
+// fixes from Paul Irish and Tino Zijdel
+ 
+(function() {
     var lastTime = 0;
-    var vendors = ['webkit', 'moz'];
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
     for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
         window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-        window.cancelAnimationFrame =
-          window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
     }
-
+ 
     if (!window.requestAnimationFrame)
         window.requestAnimationFrame = function(callback, element) {
             var currTime = new Date().getTime();
@@ -21,16 +47,20 @@ canvas = document.getElementById('myCanvas');
             lastTime = currTime + timeToCall;
             return id;
         };
-
+ 
     if (!window.cancelAnimationFrame)
         window.cancelAnimationFrame = function(id) {
             clearTimeout(id);
         };
-}());**/
+}());
 
 
 function init() {
+  console.log("|----init()")
+
   if(canvas.getContext && canvas.getContext('2d')) {
+    console.log("|-----init:getContext")
+
     context = canvas.getContext('2d'); 	//context
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -68,13 +98,14 @@ function init() {
     text3.setAttribute('class', 'text'); 
     document.getElementById("container").appendChild(text3);**/
   }
+  spawn();
 }
 
 function game_start() {
 }
 
 /** Find out the browser version and  name by **/
-/**function get_browser(){
+function get_browser(){
   var N=navigator.appName, ua=navigator.userAgent, tem;
   var M=ua.match(/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i);
   if(M && (tem= ua.match(/version\/([\.\d]+)/i))!= null) M[2]= tem[1];
@@ -88,36 +119,20 @@ function get_browser_version(){
   if(M && (tem= ua.match(/version\/([\.\d]+)/i))!= null) M[2]= tem[1];
   M=M? [M[1], M[2]]: [N, navigator.appVersion, '-?'];
   return M[1];
-}**/
-//create the particles
-
-var ps = [];
-var MAX_NUM = 500;
-var colors = [ '#69D2E7', '#A7DBD8', '#E0E4CC', '#F38630', '#FA6900', '#FF4E50', '#F9D423' ];
-
-function spawn() {
-  for(var i=0; ps.length < MAX_NUM; i++) {
-    ps[i] = { x: Math.random()*window.innerWidth,
-              y: Math.random()*window.innerHeight,
-              r: Math.random()*5,
-              c: colors[Math.floor(Math.random()*colors.length)]
-            };                  
-   }
 }
 
 function update(){
+  requestAnimationFrame(update);
+
   reset();
-  
+    
   for(var i=0; i<ps.length; i++) {
-    ctx.beginPath();
-		ctx.arc( ps[i].x, ps[i].y, ps[i].r, 0, 6);
-		ctx.fillStyle = ps[i].c;
-		ctx.fill(); 
+    context.beginPath();
+		context.arc( ps[i].x, ps[i].y, ps[i].r, 0, 6);
+		context.fillStyle = ps[i].c;
+		context.fill(); 
   }
 
-  if (!isPaused){
-      window.requestAnimationFrame(update);
-  }
 }
 
 function reset() {
@@ -125,26 +140,21 @@ function reset() {
     //reset the x and y coordinates if leaves the canvas
     for(var i=0; i<ps.length; i++) {
         //reset if y or coordinate has left the canvas
-        if(ps[i].y > c.height) {
+        if(ps[i].y > canvas.height) {
             ps[i].y = Math.random()*window.innerHeight;
             ps[i].color = colors[Math.floor(Math.random() * colors.length)];
         }
         //reset if x or coordinate has left the canvas
-        if(ps[i].x > c.width || ps[i].x < 0){
+        if(ps[i].x > canvas.width || ps[i].x < 0){
           ps[i].x = Math.random()*window.innerWidth;
           ps[i].color = colors[Math.floor(Math.random() * colors.length)];
         }
 	}
 }
-function stop() {
-	if (requestId)
-	window.cancelAnimationFrame(requestId);
-	requestId = 0;
-}
 
 window.onload = function () {  
+  console.log("--- on load -----")
   canvas.width = canvas.width;
   canvas.height = canvas.height;
   init();
-  spawn();
 };
